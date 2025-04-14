5ed61008723543e4c51c36e12ccf31588b4b290f
NataliaCarolineLopez <nataliacarolinesenai@gmail.com>
2025-04-14 16:27:00 -0300

    Update livro.php

--- livro.php
+++ livro.php
@@ -3,7 +3,7 @@ class Livro {
     // Array de livros
     private $livros = [
         ['titulo' => 'Café com Deus Pai', 'autor' => 'Junior Rostirola', 'ano' => 2020],
-        ['titulo' => 'é assim que acaba', 'autor' => 'Colleen Hoover', 'ano' => 2016],
+        ['titulo' => 'É assim que acaba', 'autor' => 'Colleen Hoover', 'ano' => 2016],
         ['titulo' => 'Racionais, Sobrevivendo no inferno', 'autor' => 'Racionais Mcs', 'ano' => 2018],
         ['titulo' => 'Dois Irmãos', 'autor' => 'Milton Hatoum', 'ano' => 2000]
     ];
