2e4eb41acc2f7749438a680b5cbd4dcad3238346
NataliaCarolineLopez <nataliacarolinesenai@gmail.com>
2025-04-14 16:25:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca Escolar</title>
-    <link rel="stylesheet" href="style.css"> <!-- Inclui o CSS -->
+    <link rel="stylesheet" href="style.css" >
 </head>
 <body>
 
@@ -13,10 +13,8 @@
         <?php
             require_once 'LivroController.php';
 
-            // Instancia o controlador
             $controller = new LivroController();
 
-            // Chama o método para listar livros
             $controller->listarLivros();
         ?>
     </div>
